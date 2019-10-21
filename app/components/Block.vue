2d8f021b4766456a7481ce06d5313e77661970fd
Maximilian Witte <maximilianwitte@outlook.de>
2019-10-22 00:10:43 +0200

    Implement basic routing, block inputs

--- app/components/Block.vue
+++ app/components/Block.vue
@@ -4,39 +4,99 @@
             <Label class="action-bar-title" text="Blocks" ></Label>
         </ActionBar>
         <StackLayout class="block-outside" backgroundColor="#2d2d2d">
-            <Label text="lol"></Label>
+            
+            <Button class="nutricional_block hide" @tap="this.data = true" :class="hide ? this.data == true : ''" height="200" width="200" backgroundColor="#3F4651"/>
+            <Button class="nutricional_block" id="fats" @tap="activateNutrient('fats')" :class="hide ? this.data == false : ''" :height="nutricional.fats" width="200" backgroundColor="#3D5A6C"/>
+            <Button class="nutricional_block" id="protein" @tap="activateNutrient('protein')" :class="hide ? this.data == false : ''" :height="nutricional.protein" width="200" backgroundColor="#FFFD82"/>
+            <Button class="nutricional_block" id="carbs" @tap="activateNutrient('carbs')" :class="hide ? this.data == false : ''" :height="nutricional.carbs" width="200" backgroundColor="#D90368"/>
+            
+            <StackLayout class="block-outside Input">
+                <Label :text="this.input_phrase" />
+                <TextField class="text_field" :text="textFieldValue" hint="35 Grams"/>
+                 <Button class="block" @tap="routeTo(Home)" text="Home" height="90" backgroundColor="#3F4651" width="300"/>
+            </StackLayout>
         </StackLayout>
     </Page>
 </template>
 
 <script>
+    import Home from "./Home.vue";
     export default {
         data() {
             return {
-                carbs: null,
-                proteins: null,
-                fat: null
+                data: true,
+                active_nutrient: null,
+                input_phrase: "Carbs: ",
+                nutricional: {
+                    carbs: 70,
+                    proteins: 50,
+                    fats: 90,
+                }
             }
         },
+        methods: {
+            tellBlockSize(carbs, proteins, fats) {
+                var block_height = 200;
+
+                var all_values = carbs + proteins + fats;
+                var values = {
+                    carbs: (carbs / all_values) * block_height,
+                    proteins: (proteins / all_values) * block_height,
+                    fats: (fats / all_values) * block_height
+                }
+                this.nutricional = values;
+                return values;
+            },
+            activateNutrient(nutrient) {
+                this.active_nutrient = nutrient;
+                this.input_phrase = "Input: " + nutrient;
+            },
+            routeTo(View) {
+                this.$navigateTo(Home, {
+                transition: {
+                    name:'fade',
+                    duration: 50
+                }
+            })
+            }
+        }
     };
 </script>
 
 <style scoped lang="scss">
-    // Start custom common variables
-    @import '../app-variables';
-    // End custom common variables
-
-    // Custom styles
-    .fa {
-        color: $accent-dark;
+    .hide {
+        visibility: collapse;
     }
-
-    .info {
-        font-size: 20;
+    .nutricional_block#carbs {
+        border-radius: 0 0 10 10;
+        transition: .3;
     }
-
-    .block {
+    .nutricional_block#protein {
+        border-radius: 0;
+        margin: 0;
+        transition: .3;
+    }
+    .nutricional_block#fats {
+        border-radius: 10 10 0 0;
+        margin-bottom: 0;
+        transition: .3;
+        margin-top: 120;
+    }
+    .nutricional_block {
+        margin-top: 5;
+        margin-bottom: 15;
+        border-radius: 10;
+        border: 0;
+        background-color: white;
+    }
+    .Input {
+        margin-top: 30;
         color: white;
-        text-align: center
+        font-size: 25;
+        text-align: center;
+    }
+    .text_field {
+        margin-top: 15;
+        color: white;
     }
 </style>
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -4,99 +4,45 @@
             <Label class="action-bar-title" text="Blocks" ></Label>
         </ActionBar>
         <StackLayout backgroundColor="#2d2d2d">    
-            <StackLayout>
-                <Button class="nutricional_block hide" :class="hide" height="200" width="200" backgroundColor="#3F4651"/>
-                <Button class="nutricional_block" id="fats" @tap="tellBlockSize(120,50,30)" :class="hide" :height="nutricional.fats" width="200" backgroundColor="#3D5A6C"/>
-                <Button class="nutricional_block" id="protein" :class="hide" :height="nutricional.protein" width="200" backgroundColor="#FFFD82"/>
-                <Button class="nutricional_block" id="carbs" :class="hide" :height="nutricional.carbs" width="200" backgroundColor="#D90368"/>
-            </StackLayout>
-            <Button class="block" @tap="$navigateTo(Block)" v-for="block in blocks" :key="block" v-bind:text="tellTime(hour, minute, block - 1)+' - '+tellTime(hour, minute, block)" height="90" backgroundColor="#3F4651"/>
+            <Button class="block" @tap="routeTo(Block)" v-for="block in blocks" :key="block" v-bind:text="tellTime(hour, minute, block - 1)+' - '+tellTime(hour, minute, block)" height="90" backgroundColor="#3F4651" width="300"/>
         </StackLayout>
     </Page>
 </template>
 
 <script>
-    import Block from "./Block";
+    import Block from "./Block.vue";
 
     export default {
-        components: {
-            Block
-        },
         data() {
             return {
                 hour: 13,
                 minute: "30",
-                blocks: 7,
-                data: true,
-                nutricional: {
-                    carbs: 70,
-                    proteins: 50,
-                    fats: 90,
-                }
+                blocks: 3
             }
         },
         methods: {
             tellTime(hour, minute, addHour) {
                 return (hour + addHour).toString() + ":" + minute;
             },
-            tellBlockSize(carbs, proteins, fats) {
-                var block_height = 200;
-
-                var all_values = carbs + proteins + fats;
-                var values = {
-                    carbs: (carbs / all_values) * block_height,
-                    proteins: (proteins / all_values) * block_height,
-                    fats: (fats / all_values) * block_height
+            routeTo(View) {
+                this.$navigateTo(Block, {
+                transition: {
+                    name:'fade',
+                    duration: 50
                 }
-                this.nutricional = values;
-                return values;
-            },
-            eval_hide(data) {
-                return hide ? data == true : ""
+            })
             }
         }
     };
 </script>
 
 <style scoped lang="scss">
-    // Start custom common variables
-    @import '../app-variables';
-    .fa {
-        color: $accent-dark;
-    }
-
-    .hide {
-        display: none;
-    }
-
     .block {
         color: white;
         text-align: center;
-        font-size: 20;
+        font-size: 25;
         font: bold;
         margin-top: 15;
-    }
-
-    .nutricional_block#carbs {
-        border-radius: 0 0 10 10;
-        transition: .3;
-    }
-    .nutricional_block#protein {
-        border-radius: 0;
-        margin: 0;
-        transition: .3;
-    }
-    .nutricional_block#fats {
-        border-radius: 10 10 0 0;
-        margin: 0;
-        transition: .3;
-    }
-
-    .nutricional_block {
-        margin-top: 5;
-        margin-bottom: 15;
-        border-radius: 10;
-        border: 0;
-        background-color: white;
+        border-radius: 15;
     }
 </style>
